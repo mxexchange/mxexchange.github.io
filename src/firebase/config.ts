1279c7d770ab2f27adc317e9a6379bdbf3aab39b
Brady Price <bradyprice1@gmail.com>
2026-01-05 06:04:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7693980200-197b7",
+  "appId": "1:1010507407944:web:cfd502e6b4af10bc5fae7d",
+  "apiKey": "AIzaSyDjkHAWSkyw1xyCO-BL4xlLF__sjHKf1XQ",
+  "authDomain": "studio-7693980200-197b7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1010507407944"
+};
